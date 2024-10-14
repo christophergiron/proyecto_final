7fe4df3775e0f1e07cbdb268e23c59460830b482
christophergiron <161170059+christophergiron@users.noreply.github.com>
2024-10-14 15:23:37 -0600

    fusion parcial de los scripts

--- Calculadora_Mate.py
+++ Calculadora_Mate.py
@@ -1,83 +1,73 @@
 import tkinter as tk
 from tkinter import messagebox
 import math
-import Pantalla_principal as PP
-rai = tk.Tk()
-rai.title("Calculadora de Combinaciones y Repeticiones")
-rai.geometry("500x500")
+# Función que coloca los widgets en el frame de matemática discreta
+def operaciones_matematica_discreta(frame):
+    camb = tk.StringVar(value="")
+    camb_rep = tk.StringVar(value="Sin Repeticion")
 
-#cambia la pantalla
-camb = tk.StringVar(value="")
-camb_rep = tk.StringVar(value="Sin Repeticion") # cambia para cambiar las repeticiones 
+    def permu(pri, seg, rep):
+        if rep == "Con Repeticion":
+            return pri ** seg
+        return math.factorial(pri) // math.factorial(pri - seg)
 
-def permu(pri, seg, rep):
-    if rep == "Con Repeticion":
-        return  (pri ** seg)
-    return math.factorial(pri) // math.factorial(pri - seg)
+    def comb(pri, seg, rep):
+        if rep == "Con Repeticion":
+            return math.factorial(pri + seg - 1) // (math.factorial(seg) * math.factorial(pri - 1))
+        return math.factorial(pri) // (math.factorial(seg) * math.factorial(pri - seg))
 
-def comb(pri, seg, rep):
-    if rep == "Con Repeticion":
-        return math.factorial(pri + seg - 1) // (math.factorial(seg) * math.factorial(pri - 1))
-    return math.factorial(pri) // (math.factorial(seg)* math.factorial(pri - seg)) #Define las ecuaciones
+    def calc():
+        try:
+            pri = int(entra_pri.get())
+            seg = int(entra_seg.get())
 
-def calc(): #Define la funcion de calcular
-    try:
-        pri = int(entra_pri.get())
-        seg = int(entra_seg.get())
-        
-        if pri < 0 or seg < 0 :
-            messagebox.showerror("","Ingrese un Valor entero valido que no sea negativo")
+            if pri < 0 or seg < 0:
+                messagebox.showerror("", "Ingrese un Valor entero válido que no sea negativo")
+                return
+
+            if seg > pri and camb_rep.get() == "Sin Repeticion":
+                messagebox.showerror("", "El segundo Valor no puede ser mayor al primero si se elige sin repetición")
+                return
+
+            if camb.get() == "Permutacion":
+                resulta = permu(pri, seg, camb_rep.get())
+                result.config(text=f"Permutaciones: {resulta}")
+            elif camb.get() == "Combinacion":
+                resulta = comb(pri, seg, camb_rep.get())
+                result.config(text=f"Combinaciones: {resulta}")
+
+        except ValueError:
+            messagebox.showerror("", "El valor introducido no es válido. Introduce por favor un número.")
             return
-        
-        if seg > pri and camb_rep.get() == "Sin Repeticion":
-            messagebox.showerror("", "El segundo Valor No puede Ser mayor al Primero Si se elige sin repeticion")
-            return
-        
-        if camb.get() == "Permutacion":
-            resulta = permu(pri, seg, camb_rep.get())
-            result.config(text = f"Permutaciones: {resulta} " ) 
-        
-        elif camb.get() == "Combinacion":
-            resulta = comb(pri, seg, camb_rep.get())
-            result.config(text = f"Combinaciones: {resulta}" ) 
-            
-    except ValueError:
-        messagebox.showerror("", "El valor introducido no es valido, Introduce Por favor un Numero")
-        return
 
-def mostrar_panta(camb2): # define el cambio de pantallas
-    camb.set(camb2)
-    result.config(text="")
-    
-    prilab.place(x=50, y=150)
-    entra_pri.place(x=200, y=150, width=200)
-    seglab.place(x=50, y=200)
-    entra_seg.place(x=200, y=200, width=200)
-    rep.place(x=50, y=250)
-    conrep.place(x=275, y=250)
-    norep.place(x=100, y=250)
-    btncalc.place(x=50, y=350)
-    result.place(x=50, y=400)
-    
-prilab = tk.Label(rai, text="Ingrese El Primer Valor: ", font=("Times New Roman", 10))
-seglab = tk.Label(rai, text="Ingrese El segundo valor", font=("Times New Roman", 10))
-entra_pri = tk.Entry(rai, font=("Times New Roman", 10), width=20)
-entra_seg = tk.Entry(rai, font=("Times New Roman", 10), width=20)
+    def mostrar_panta(camb2):
+        camb.set(camb2)
+        result.config(text="")
 
-rep = tk.Label(rai, text="Tipo:", font=("Times New Roman", 10))
-norep = tk.Radiobutton(rai, text="Sin Repeticion", variable=camb_rep, value="Sin Repeticion", font=("Times New Roman", 10))
-conrep = tk.Radiobutton(rai, text="Con Repeticion", variable=camb_rep, value="Con Repeticion", font=("Times New Roman", 10))
+        prilab.grid(row=1, column=0, padx=10, pady=10)
+        entra_pri.grid(row=1, column=1, padx=10, pady=10)
+        seglab.grid(row=2, column=0, padx=10, pady=10)
+        entra_seg.grid(row=2, column=1, padx=10, pady=10)
+        rep.grid(row=3, column=0, padx=10, pady=10)
+        norep.grid(row=3, column=1, padx=10, pady=10)
+        conrep.grid(row=4, column=1, padx=10, pady=10)
+        btncalc.grid(row=5, column=0, columnspan=2, pady=10)
+        result.grid(row=6, column=0, columnspan=2, pady=10)
 
-btncalc = tk.Button(rai, text="Calcular", command=calc, font=("Times New Roman", 10), width=12, height=3)
+    prilab = tk.Label(frame, text="Ingrese El Primer Valor:", font=("Times New Roman", 10))
+    seglab = tk.Label(frame, text="Ingrese El Segundo Valor:", font=("Times New Roman", 10))
+    entra_pri = tk.Entry(frame, font=("Times New Roman", 10), width=20)
+    entra_seg = tk.Entry(frame, font=("Times New Roman", 10), width=20)
 
-result = tk.Label(rai, text="", font=("Times New Roman", 10))
+    rep = tk.Label(frame, text="Tipo:", font=("Times New Roman", 10))
+    norep = tk.Radiobutton(frame, text="Sin Repeticion", variable=camb_rep, value="Sin Repeticion", font=("Times New Roman", 10))
+    conrep = tk.Radiobutton(frame, text="Con Repeticion", variable=camb_rep, value="Con Repeticion", font=("Times New Roman", 10))
 
-btnpermu = tk.Button(rai, text="Permutaciones", command=lambda: mostrar_panta ("Permutacion"), font=("Times New Roman", 10), width=20, height=2)
-btncomb = tk.Button(rai, text="Combinaciones", command=lambda: mostrar_panta("Combinacion"), font=("Times New Roman", 10), width=20, height=2)
+    btncalc = tk.Button(frame, text="Calcular", command=calc, font=("Times New Roman", 10), width=12, height=3)
+    result = tk.Label(frame, text="", font=("Times New Roman", 10))
 
-btnpermu.place(x=50, y=20)
-btncomb.place (x=250, y=20)
+    btnpermu = tk.Button(frame, text="Permutaciones", command=lambda: mostrar_panta("Permutacion"), font=("Times New Roman", 10), width=20, height=2)
+    btncomb = tk.Button(frame, text="Combinaciones", command=lambda: mostrar_panta("Combinacion"), font=("Times New Roman", 10), width=20, height=2)
 
-PP.volver()
-
-rai.mainloop()
+    btnpermu.grid(row=0, column=0, padx=10, pady=10)
+    btncomb.grid(row=0, column=1, padx=10, pady=10)
--- Pantalla_principal.py
+++ Pantalla_principal.py
@@ -1,20 +1,24 @@
 import tkinter as tk
 import Calculadora_Mate as CM
 
+ventana = tk.Tk()
+ventana.title("proyecto final")
+ventana.geometry("600x500")
+ventana.config()
+
 def pantalla_inicio():
     home.grid()
     algebra_lineal.grid_forget()
     matematica_discreta.grid_forget()
-    
+
 def pantalla_algebra():
     algebra_lineal.grid()
-
     home.grid_forget()
     matematica_discreta.grid_forget()
     boton1.place_forget()
     boton2.place_forget()
     boton3.place_forget()
-    
+
 def pantalla_matematica():
     matematica_discreta.grid()
     home.grid_forget()
@@ -23,41 +27,26 @@ def pantalla_matematica():
     boton2.place_forget()
     boton3.place_forget()
 
-ventana = tk.Tk()
-ventana.title("proyecto final")
-ventana.geometry("400x300")
+    # limpia cualquier cosa en el frame para agregar correctamente los del script de matematica
+    for widget in matematica_discreta.winfo_children():
+        widget.destroy() #limpia el contenido del frame
+    CM.operaciones_matematica_discreta(matematica_discreta) #se carga la funcion del script de matematica
 
 home = tk.Frame(ventana)
 algebra_lineal = tk.Frame(ventana)
-matematica_discreta= tk.Frame(ventana)
+matematica_discreta = tk.Frame(ventana)
 
-boton1 = tk.Button(ventana,text=" Inicio",command=pantalla_inicio)
-boton1.place(x=145,y=40, width=130, height=30)
+boton1 = tk.Button(ventana, text="Inicio", command=pantalla_inicio)
+boton1.place(x=250, y=70, width=120, height=25)
 
-boton2 = tk.Button(ventana, text=" Alebra lineal ", command=pantalla_algebra)
-boton2.place(x=145, y=70, width=130, height=30)
+boton2 = tk.Button(ventana, text="Agebra lineal", command=pantalla_algebra)
+boton2.place(x=250, y=100, width=120, height=25)
 
-boton3 =tk.Button(ventana,text="Matematica discreta", command=pantalla_matematica)
-boton3.place(x=145, y=100, width=130, height=30)
-def volver():
-    boton4 = tk.Button(ventana, text=" regresar al inicio ", command=pantalla_inicio)
-    boton4.place(x=145, y=150, width=130, height=30)
-#opciones de menus 
-#opciones de la pagina 1
-poyo= tk.Label(home, text="Bienvenido selecciona tu calculadora",font=("Times New Roman", 12))
-poyo.grid(row=0,column=0,padx=108, pady=10)
+boton3 = tk.Button(ventana, text="Matematica discreta", command=pantalla_matematica)
+boton3.place(x=250, y=130, width=120, height=25)
 
-titulo1=tk.Label(matematica_discreta, text="poyo")
-titulo1.grid(row=2,column=1)
-
-entrada1=tk.Entry(matematica_discreta)
-entrada1.grid(row=3, column=1)
-
-titulo2 =tk.Label(algebra_lineal, text="poy")
-titulo2.grid(row=2,column=1)
-
-entrada2=tk.Entry(algebra_lineal)
-entrada2.grid(row=3, column=1)
+bienvenida = tk.Label(home, text="Bienvenido, selecciona tu calculadora", font=("Times New Roman", 12),)
+bienvenida.grid(row=0, column=2, padx=200, pady=20)
 
 pantalla_inicio()
 

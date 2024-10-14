b2e535fc32de4c493075740fb4ac80b1295107c6
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2024-10-14 11:35:53 -0600

    Inicio de la calculadora de matrices

---
+++ Calculadora_Algebra.py
@@ -0,0 +1,9 @@
+import Pantalla_principal as PP
+import tkinter as tk
+from tkinter import messagebox
+import math
+
+ventana = tk.Tk()
+ventana.title("Calculadora de matrices")
+ventana.geometry("400x300")
+

44b5cc771a649e17bbea9c93990ad651a19cd460
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2024-10-14 14:46:31 -0600

    Primeros botones

--- Calculadora_Algebra.py
+++ Calculadora_Algebra.py
@@ -4,12 +4,31 @@ from tkinter import messagebox
 import math
 import numpy
 
-ventana = tk.Tk()
-ventana.title("Calculadora de matrices")
-ventana.geometry("400x300")
 
-instruccion = tk.Label(ventana, text="Seleccione la operacion a realizar: ")
+def inversa():
+    print("Test")
+    
+def multi():
+    print("Test")
+    
+def sis_ecu():
+    print("Test")
+    
+CA_Win = tk.Tk()
+CA_Win.title("Calculadora de matrices")
+CA_Win.geometry("400x300")
+
+instruccion = tk.Label(CA_Win, text="Seleccione la operacion a realizar: ")
 instruccion.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+boton = tk.Button(CA_Win, text="Matriz inversa", command=inversa)
+boton.grid(row=1, column=8, padx=10, pady=10)
+
+boton = tk.Button(CA_Win, text="Multiplicacion", command=multi)
+boton.grid(row=11, column=8, padx=10, pady=10)
+
+boton = tk.Button(CA_Win, text="Sistemas de ecuaciones lineales", command=sis_ecu)
+boton.grid(row=21, column=8, padx=10, pady=10)
 #pp.volver()
 
-ventana.mainloop()
+CA_Win.mainloop()

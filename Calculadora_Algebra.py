4d0cc10b4de2020024a30c0b25477130b10276d7
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2024-10-14 12:09:35 -0600

    Update Calculadora_Algebra.py

--- Calculadora_Algebra.py
+++ Calculadora_Algebra.py
@@ -1,10 +1,15 @@
-import Pantalla_principal as PP
+#import Pantalla_principal as PP
 import tkinter as tk
 from tkinter import messagebox
 import math
+import numpy
 
 ventana = tk.Tk()
 ventana.title("Calculadora de matrices")
 ventana.geometry("400x300")
 
+instruccion = tk.Label(ventana, text="Seleccione la operacion a realizar: ")
+instruccion.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 #pp.volver()
+
+ventana.mainloop()

a8562c6ee40d5f2ea344ed03b2667916a044aefc
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2024-10-14 15:42:38 -0600

    Botones segunda parte

--- Calculadora_Algebra.py
+++ Calculadora_Algebra.py
@@ -1,22 +1,44 @@
 #import Pantalla_principal as PP
 import tkinter as tk
-from tkinter import messagebox
-import math
-import numpy
-
+from tkinter import messagebox 
+import numpy as np
 
 def inversa():
-    print("Test")
+    instruccion.grid_remove()
+    boton.grid_remove()
+    botonS.grid_remove()
+    botonM.grid_remove()
+    inversaExec()
     
 def multi():
-    print("Test")
+    instruccion.grid_remove()
+    boton.grid_remove()
+    botonS.grid_remove()
+    botonM.grid_remove()
+    multiExec()
     
 def sis_ecu():
-    print("Test")
+    instruccion.grid_remove()
+    boton.grid_remove()
+    botonS.grid_remove()
+    botonM.grid_remove()
+    sis_ecuExec()
+
+def inversaExec():
+    instruccionInv = tk.Label(CA_Win, text="Ingrese su matriz: ")
+    instruccionInv.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
     
+def multiExec():
+    instruccionInv = tk.Label(CA_Win, text="Ingrese su matriz: ")
+    instruccionInv.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+    
+def sis_ecuExec():
+    instruccionSis = tk.Label(CA_Win, text="Ingrese su matriz: ")
+    instruccionSis.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        
 CA_Win = tk.Tk()
 CA_Win.title("Calculadora de matrices")
-CA_Win.geometry("400x300")
+CA_Win.geometry("625x300")
 
 instruccion = tk.Label(CA_Win, text="Seleccione la operacion a realizar: ")
 instruccion.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
@@ -24,11 +46,11 @@ instruccion.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 boton = tk.Button(CA_Win, text="Matriz inversa", command=inversa)
 boton.grid(row=1, column=8, padx=10, pady=10)
 
-boton = tk.Button(CA_Win, text="Multiplicacion", command=multi)
-boton.grid(row=11, column=8, padx=10, pady=10)
+botonM = tk.Button(CA_Win, text="Multiplicacion", command=multi)
+botonM.grid(row=11, column=8, padx=10, pady=10)
 
-boton = tk.Button(CA_Win, text="Sistemas de ecuaciones lineales", command=sis_ecu)
-boton.grid(row=21, column=8, padx=10, pady=10)
+botonS = tk.Button(CA_Win, text="Sistemas de ecuaciones lineales", command=sis_ecu)
+botonS.grid(row=21, column=8, padx=10, pady=10)
 #pp.volver()
 
 CA_Win.mainloop()
